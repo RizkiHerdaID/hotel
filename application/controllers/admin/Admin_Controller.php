0fa93c35f987a6e4a9786b0bcc39d679b0161dc7
Rizki Herdatullah <rizkiherda@gmail.com>
2016-08-07 14:21:11 +0700

    Admin Cntroller modified

--- application/controllers/admin/Admin_Controller.php
+++ application/controllers/admin/Admin_Controller.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Admin_Controller extends My_Controller {
-
+	
 	public function __construct()
 	{
 		parent::__construct();
