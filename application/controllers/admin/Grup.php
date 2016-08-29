7ac7598d792b2b57775fdd0085beafa63ddf3ab4
RIZKI-LAPTOP\Rizki Herdatullah <rizkiherda@gmail.com>
2016-08-30 02:44:24 +0700

    Bugs Fixed - Data Grup Tamu on #issue6 & #issue8

--- application/controllers/admin/Grup.php
+++ application/controllers/admin/Grup.php
@@ -1,74 +1,70 @@
-<?php 
+<?php
 
-class Grup extends CI_Controller {
-	
+class Grup extends CI_Controller
+{
+
     var $template = 'admin/template';
-    
-	private $table = 'guest_group';
-	private $pk = 'kode_grup';
+    private $pk = 'kode_grup';
 
-	public function __construct()
-	{
-		parent::__construct();
-        if (!$this->authentication->is_loggedin())
-        {
+    public function __construct()
+    {
+        parent::__construct();
+        if (!$this->authentication->is_loggedin()) {
             redirect('auth');
         }
-		$this->load->model('m_grup', 'm_grup');
-	}
-
-	public function index()
-	{
-		$data = [
-			'title' => 'Data Grup Tamu',
-			'content' => 'admin/grup/index',
-			'grup' => $this->m_grup->read()
-		];
-		$this->load->view($this->template, $data);
-	}
-
-    public function details($id){
-        $data = [
-            'title' => 'Data Detail Grup Tamu',
-            'content' => 'admin/grup/detail',
-            'grup' => $this->m_grup->read($id)
-        ];
-        $this->load->view($this->template, $data);
+        $this->load->model('m_grup', 'm_grup');
     }
 
-    public function viewUpdate($id){
+    public function index($back=FALSE)
+    {
+        $back = $this->input->post('back');
+        $content = 'admin/grup/index';
+        $data = [
+            'title' => 'Data Grup Tamu',
+            'content' => $content,
+            'grup' => $this->m_grup->read()
+        ];
+        if ($back){
+            $response = $this->load->view($content, $data, TRUE);
+            echo $response;
+        } else {
+            $this->load->view($this->template, $data);
+        }
+    }
+
+    public function viewUpdate()
+    {
+        $id = $this->input->post('id');
+        $content = 'admin/grup/update';
         $data = [
             'title' => 'Update Data Grup Tamu',
-            'content' => 'admin/grup/update',
             'grup' => $this->m_grup->read($id)
         ];
-        $this->load->view($this->template, $data);
+        $response = $this->load->view($content, $data, TRUE);
+        echo $response;
     }
 
-	public function create(){
-		if($this->input->server('REQUEST_METHOD') == "POST")
-        {
-        	$this->form_validation->set_rules('gcode', 'Kode Grup', 'trim|required|min_length[4]|max_length[5]');
+    public function create()
+    {
+        if ($this->input->server('REQUEST_METHOD') == "POST") {
+            $this->form_validation->set_rules('gcode', 'Kode Grup', 'trim|required|max_length[5]');
             $this->form_validation->set_rules('gname', 'Nama Grup', 'trim|required|max_length[50]');
-            $this->form_validation->set_rules('diskon', 'Nama Depan', 'trim|required|is_natural');
-          	if ($this->form_validation->run() == FALSE)
-            {
-            	$this->session->set_flashdata("errors", validation_errors());
-            } 
-            else 
-            {
-            	$data = [
+            $this->form_validation->set_rules('diskon', 'Diskon', 'trim|required|is_natural');
+            if ($this->form_validation->run() == FALSE) {
+                $this->session->set_flashdata("errors", validation_errors());
+            } else {
+                $data = [
                     'kode_grup' => $this->input->post('gcode'),
                     'nama' => $this->input->post('gname'),
                     'diskon' => $this->input->post('diskon')
                 ];
 
-                if($this->m_grup->create($data)){
-                	$this->session->set_flashdata("operation", "success");
+                if ($this->m_grup->create($data)) {
+                    $this->session->set_flashdata("operation", "success");
                     $this->session->set_flashdata("message", "<strong>Grup Tamu</strong> berhasil ditambah");
                     redirect('admin/grup');
                 } else {
-                	$data = [
+                    $data = [
                         "operation" => "warning",
                         "message" => "Maaf. Terjadi kesalahan sistem.",
                     ];
@@ -77,34 +73,31 @@ class Grup extends CI_Controller {
         }
 
         $data = [
-			'title' => 'Data Grup Tamu',
-			'content' => 'admin/grup/index',
-			'grup' => $this->m_grup->read()
-		];
+            'title' => 'Data Grup Tamu',
+            'content' => 'admin/grup/index',
+            'grup' => $this->m_grup->read()
+        ];
         $this->load->view($this->template, $data);
-	}
+    }
 
-    public function update(){
+    public function update()
+    {
         $id_guest_group = $this->input->post('id');
-        if($this->input->server('REQUEST_METHOD') == "POST")
-        {
+        if ($this->input->server('REQUEST_METHOD') == "POST") {
             $this->form_validation->set_rules('gname', 'Nama Grup', 'trim|required|max_length[50]');
             $this->form_validation->set_rules('gcode', 'Kode Grup', 'trim|required|max_length[50]');
             $this->form_validation->set_rules('diskon', 'Nama Depan', 'trim|required|is_natural');
-            if ($this->form_validation->run() == FALSE)
-            {
+            if ($this->form_validation->run() == FALSE) {
                 //ERROR
                 $this->session->set_flashdata("errors", validation_errors());
-            } 
-            else 
-            {
+            } else {
                 $data = [
                     'kode_grup' => $this->input->post('gcode'),
                     'nama' => $this->input->post('gname'),
                     'diskon' => $this->input->post('diskon')
                 ];
 
-                if($this->m_grup->update($data, $id_guest_group)){
+                if ($this->m_grup->update($data, $id_guest_group)) {
                     $this->session->set_flashdata("operation", "success");
                     $this->session->set_flashdata("message", "<strong>Grup Tamu</strong> berhasil di update");
                     redirect('admin/grup');
@@ -125,18 +118,17 @@ class Grup extends CI_Controller {
         $this->load->view($this->template, $data);
     }
 
-	public function delete($id)
+    public function delete($id)
     {
         $result = $this->m_grup->delete($id);
-        if($result){
+        if ($result) {
             $this->session->set_flashdata("operation", "success");
             $this->session->set_flashdata("message", "<strong>Berhasil</strong> menghapus grup tamu");
-        }
-        else{
+        } else {
             $this->session->set_flashdata("operation", "danger");
             $this->session->set_flashdata("message", "<strong>Gagal</strong> Terjadi kesalah sistem.");
         }
-        redirect("admin/grup");
+        redirect('admin/grup');
     }
 
 }
