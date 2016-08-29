a3aafd51d21cc2c9466b322f454a1e96ef55370d
RIZKI-LAPTOP\Rizki Herdatullah <rizkiherda@gmail.com>
2016-08-30 02:46:22 +0700

    Data Jenis Kamar on #issue18 & issue19

--- application/controllers/admin/Jenis.php
+++ application/controllers/admin/Jenis.php
@@ -1,129 +1,140 @@
-<?php 
+<?php
 
-class Jenis extends CI_Controller {
+class Jenis extends CI_Controller
+{
 
-	var $template = 'admin/template';
-	
-	public function __construct()
-	{
-		parent::__construct();
-        if (!$this->authentication->is_loggedin())
-        {
+    var $template = 'admin/template';
+
+    public function __construct()
+    {
+        parent::__construct();
+        if (!$this->authentication->is_loggedin()) {
             redirect('auth');
         }
-		$this->load->model('m_jenis');
+        $this->load->model('m_jenis');
         $this->load->model('m_jasa');
-	}
-
-	public function index()
-	{	
-		$facilities = ['AC', 'TV', 'Hot Water', 'Kulkas'];
-		$data = [
-			'title' => 'Data Jenis Kamar',
-			'content' => 'admin/jenis/index',
-			'jenis' => $this->m_jenis->read(),
-			'tahun' => $this->getTahun(),
-			'facilities' => $this->m_jasa->read_jk()
-		];
-		$this->load->view($this->template, $data);
-	}
-
-	public function details($id){
-        $data = [
-            'title' => 'Detail Data Jenis Kamar',
-            'content' => 'admin/jenis/detail',
-            'jenis' => $this->m_jenis->read($id)
-        ];
-        $this->load->view($this->template, $data);
     }
 
-	public function viewUpdate($id){
-		$facilities = ['AC', 'TV', 'Hot Water', 'Kulkas'];
+    public function index($back = FALSE)
+    {
+        $back = $this->input->post('back');
+        $content = 'admin/jenis/index';
+        $facilities = ['AC', 'TV', 'Hot Water', 'Kulkas'];
+        $data = [
+            'title' => 'Data Jenis Kamar',
+            'content' => $content,
+            'jenis' => $this->m_jenis->read(),
+            'tahun' => $this->getTahun(),
+            'facilities' => $this->m_jasa->read_jk()
+        ];
+        if ($back) {
+            $respone = $this->load->view($content, $data, TRUE);
+            echo $respone;
+        } else {
+            $this->load->view($this->template, $data);
+        }
+    }
+
+    public function getTahun()
+    {
+        $datestring = '%Y';
+        $time = time();
+        return mdate($datestring, $time);
+    }
+
+    public function details()
+    {
+        $id = $this->input->post('id');
+        $content = 'admin/jenis/detail';
+        $data = [
+            'title' => 'Detail Data Jenis Kamar',
+            'jenis' => $this->m_jenis->read($id)
+        ];
+        $response = $this->load->view($content, $data, TRUE);
+        echo $response;
+    }
+
+    public function viewUpdate()
+    {
+        $id = $this->input->post('id');
+        $content = 'admin/jenis/update';
         $data = [
             'title' => 'Update Data Jenis Kamar',
-            'content' => 'admin/jenis/update',
             'jenis' => $this->m_jenis->read($id),
             'tahun' => $this->getTahun(),
             'facilities' => $this->m_jasa->read_jk()
         ];
-        $this->load->view($this->template, $data);
-    }    
+        $response = $this->load->view($content, $data, TRUE);
+        echo $response;
+    }
 
-    public function create(){
-		if($this->input->server('REQUEST_METHOD') == "POST")
-        {
-        	$this->form_validation->set_rules('kode', 'Kode Jenis', 'trim|required|min_length[4]|max_length[6]');
+    public function create()
+    {
+        if ($this->input->server('REQUEST_METHOD') == "POST") {
+            $this->form_validation->set_rules('kode', 'Kode Jenis', 'trim|required|min_length[4]|max_length[6]');
             $this->form_validation->set_rules('jenis', 'Nama Jenis Kamar', 'trim|required|max_length[40]');
             $this->form_validation->set_rules('harga', 'Harga Kamar', 'required|max_length[100]|is_natural');
             $this->form_validation->set_rules('tahun', 'Tahun', 'required|max_length[4]|is_natural');
 
-          	if ($this->form_validation->run() == FALSE)
-            {
-            	//ERROR
-               $this->session->set_flashdata("errors", validation_errors());
-            } 
-            else 
-            {	
-            	$facilities = $this->input->post('facilities');
-            	$data = [
+            if ($this->form_validation->run() == FALSE) {
+                //ERROR
+                $this->session->set_flashdata("errors", validation_errors());
+            } else {
+                $facilities = $this->input->post('facilities');
+                $data = [
                     'kode_jenis' => $this->input->post('kode'),
                     'title' => $this->input->post('jenis'),
                     'price' => $this->input->post('harga'),
                     'tahun' => $this->input->post('tahun'),
-                    'fasilitas' => implode(",", $facilities) 
+                    'fasilitas' => implode(",", $facilities)
                 ];
-                
-                if($this->m_jenis->create($data)) {
-                	$this->session->set_flashdata("operation", "success");
+
+                if ($this->m_jenis->create($data)) {
+                    $this->session->set_flashdata("operation", "success");
                     $this->session->set_flashdata("message", "<strong>Data Jenis Kamar</strong> berhasil ditambah");
                     redirect('admin/jenis');
                 } else {
-                	$data = [
+                    $data = [
                         "operation" => "warning",
                         "message" => "Maaf. Terjadi kesalahan sistem.",
                     ];
                 }
             }
         }
-
-        $facilities = ['AC', 'TV', 'Hot Water', 'Kulkas'];
         $data = [
-			'title' => 'Data Tamu',
-			'content' => 'admin/jenis/index',
-			'jenis' => $this->m_jenis->read(),
-			'tahun' => $this->getTahun(),
-			'facilities' => $facilities
-		];
+            'title' => 'Data Jenis Kamar',
+            'content' => 'admin/jenis/index',
+            'jenis' => $this->m_jenis->read(),
+            'tahun' => $this->getTahun(),
+            'facilities' => $this->m_jasa->read_jk()
+        ];
         $this->load->view($this->template, $data);
-	}
+    }
 
-	public function update(){
-		$idclass = $this->input->post('idclass');
-		if($this->input->server('REQUEST_METHOD') == "POST")
-        {
+    public function update()
+    {
+        $idclass = $this->input->post('idclass');
+        if ($this->input->server('REQUEST_METHOD') == "POST") {
             $this->form_validation->set_rules('jenis', 'Nama Jenis Kamar', 'trim|required|max_length[40]');
             $this->form_validation->set_rules('harga', 'Harga Kamar', 'required|max_length[100]|is_natural');
             $this->form_validation->set_rules('tahun', 'Tahun', 'required|max_length[4]|is_natural');
 
-          	if ($this->form_validation->run() == FALSE)
-            {
-            	$this->session->set_flashdata("errors", validation_errors());
-            } 
-            else 
-            {	
-            	$facilities = $this->input->post('facilities');
-            	$data = [
+            if ($this->form_validation->run() == FALSE) {
+                $this->session->set_flashdata("errors", validation_errors());
+            } else {
+                $facilities = $this->input->post('facilities');
+                $data = [
                     'title' => $this->input->post('jenis'),
                     'price' => $this->input->post('harga'),
                     'tahun' => $this->input->post('tahun'),
-                    'fasilitas' => implode(",", $facilities) 
+                    'fasilitas' => implode(",", $facilities)
                 ];
-                if($this->m_jenis->update($idclass, $data)) {
-                	$this->session->set_flashdata("operation", "success");
+                if ($this->m_jenis->update($idclass, $data)) {
+                    $this->session->set_flashdata("operation", "success");
                     $this->session->set_flashdata("message", "<strong>Data Jenis Kamar</strong> berhasil di update");
                     redirect('admin/jenis');
                 } else {
-                	$data = [
+                    $data = [
                         "operation" => "warning",
                         "message" => "Maaf. Terjadi kesalahan sistem.",
                     ];
@@ -133,34 +144,27 @@ class Jenis extends CI_Controller {
 
         $facilities = ['AC', 'TV', 'Hot Water', 'Kulkas'];
         $data = [
-			'title' => 'Data Tamu',
-			'content' => 'admin/jenis/update',
-			'jenis' => $this->m_jenis->read($idclass),
-			'tahun' => $this->getTahun(),
-			'facilities' => $facilities
-		];
+            'title' => 'Data Jenis Kamar',
+            'content' => 'admin/jenis/update',
+            'jenis' => $this->m_jenis->read($idclass),
+            'tahun' => $this->getTahun(),
+            'facilities' => $this->m_jasa->read_jk()
+        ];
         $this->load->view($this->template, $data);
-	}
+    }
 
-	public function delete($id)
+    public function delete($id)
     {
         $result = $this->m_jenis->delete($id);
-        if($result){
+        if ($result) {
             $this->session->set_flashdata("operation", "success");
             $this->session->set_flashdata("message", "<strong>Berhasil</strong> menghapus data Jenis Kamar");
-        }
-        else{
+        } else {
             $this->session->set_flashdata("operation", "danger");
             $this->session->set_flashdata("message", "<strong>Gagal</strong> Terjadi kesalah sistem.");
         }
         redirect("admin/jenis");
     }
-
-    public function getTahun(){
-		$datestring = '%Y';
-		$time = time();
-		return mdate($datestring, $time);
-	}
 }
 
 /* End of file  */
--- application/views/admin/jenis/index.php
+++ application/views/admin/jenis/index.php
@@ -1,169 +1,194 @@
 <div class="row">
-	<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
-		<div class="panel panel-warning">
-			<!-- Default panel contents -->
-			<div class="panel-heading"><h4><?=$title?></h4></div>
-			<?php if($this->session->flashdata('operation') != NULL){ ?>
-			<div class="alert alert-<?=$this->session->flashdata('operation')?>" role="alert">
-				<p><?=$this->session->flashdata('message'); ?></p>
-			</div>
-			<?php } ?>
-			<!-- Table -->
-			<table class="table">
-				<table class="table">
-					<thead>
-						<tr>
-							<th>No</th>
-							<th>No. Jenis</th>
-							<th>Kode Kamar</th>
-							<th>Harga</th>
-							<th>Action</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php $no = 1; ?>
-						<?php foreach($jenis as $list): ?>
-						<tr>
-							<td><?=$no++?></td>
-							<td><?=$list['kode_jenis']?></td>
-							<td><?=$list['title']?></td>
-							<td><?php echo 'Rp. ' . number_format($list['price'], '0' , '' , '.' ) . ',-'; ?></td>
-							<td>
-								<button class="btn btn-warning btn-xs" onclick="detailData('<?=$list["idclass"]?>')">Detail</button>
-								<button class="btn btn-success btn-xs" onclick="updateData('<?=$list["idclass"]?>')">Update</button>
-								<button class="btn btn-danger btn-xs btn-delete" onclick="confirmDeleteModal('<?=$list["idclass"]?>')">
-								Hapus
-								</button>
-							</td>
-						</tr>
-						<?php endforeach; ?>
-					</tbody>
-				</table>
-			</div>
-		</div>
-		<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
-			<div class="panel panel-warning">
-				<div class="panel-heading"><h4>Tambah Jenis Kamar</h4></div>
-				<div class="panel-body">
-                    
-                    <!-- alert -->
-                    <?php if($this->session->flashdata('errors') != NULL){ ?>
-                        <div class="alert alert-warning" role="alert">
-                            <p><?=$this->session->flashdata('errors'); ?></p>
+    <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+        <div class="panel panel-warning">
+            <!-- Default panel contents -->
+            <div class="panel-heading"><h4><?= $title ?></h4></div>
+            <?php if ($this->session->flashdata('operation') != NULL) { ?>
+                <div class="alert alert-<?= $this->session->flashdata('operation') ?>" role="alert">
+                    <p><?= $this->session->flashdata('message'); ?></p>
+                </div>
+            <?php } ?>
+            <!-- Table -->
+            <table class="table">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>No. Jenis</th>
+                        <th>Kode Kamar</th>
+                        <th>Harga</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $no = 1; ?>
+                    <?php foreach ($jenis as $list): ?>
+                        <tr>
+                            <td><?= $no++ ?></td>
+                            <td><?= $list['kode_jenis'] ?></td>
+                            <td><?= $list['title'] ?></td>
+                            <td><?php echo 'Rp. ' . number_format($list['price'], '0', '', '.') . ',-'; ?></td>
+                            <td>
+                                <button class="btn btn-warning btn-xs" onclick="detailData('<?= $list["idclass"] ?>')">
+                                    Detail
+                                </button>
+                                <button class="btn btn-success btn-xs" onclick="updateData('<?= $list["idclass"] ?>')">
+                                    Update
+                                </button>
+                                <button class="btn btn-danger btn-xs btn-delete"
+                                        onclick="confirmDeleteModal('<?= $list["idclass"] ?>')">
+                                    Hapus
+                                </button>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+        </div>
+    </div>
+    <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+        <div class="panel panel-warning">
+            <div class="panel-heading"><h4>Tambah Jenis Kamar</h4></div>
+            <div class="panel-body">
+
+                <!-- alert -->
+                <?php if ($this->session->flashdata('errors') != NULL) { ?>
+                    <div class="alert alert-warning" role="alert">
+                        <p><?= $this->session->flashdata('errors'); ?></p>
+                    </div>
+                <?php } ?>
+                <!-- end of alert -->
+
+                <form action="<?= site_url('admin/jenis/create') ?>" method="post" class="form-horizontal"
+                      enctype="multipart/form-data" role="form">
+                    <fieldset>
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="kode">Kode Jenis</label>
+                            <div class="col-md-3">
+                                <input id="kode" maxlength="6" name="kode" type="text" placeholder=""
+                                       value="<?= set_value('kode') ?>" class="form-control input-md" required="">
+                            </div>
+                            <label class="col-md-5" style="margin-top:5pt;">*maksimal 6 karakter</label>
                         </div>
-                    <?php } ?>
-                    <!-- end of alert -->
-                    
-					<form action="<?=site_url('admin/jenis/create')?>" method="post" class="form-horizontal" enctype="multipart/form-data" role="form">
-						<fieldset>
-							<!-- Text input-->
-							<div class="form-group">
-								<label class="col-md-4 control-label" for="kode">Kode Jenis</label>
-								<div class="col-md-3">
-									<input id="kode" maxlength="6" name="kode" type="text" placeholder="" value="<?=set_value('kode')?>" class="form-control input-md" required="">
-								</div>
-								<label class="col-md-5" style="margin-top:5pt;">*maksimal 6 karakter</label>
-							</div>
-							<div class="form-group">
-								<label class="col-md-4 control-label" for="jenis">Nama Jenis Kamar</label>
-								<div class="col-md-8">
-									<input id="jenis" maxlength="40" name="jenis" type="text" placeholder="" value="<?=set_value('jenis')?>" class="form-control input-md" required="">
-								</div>
-							</div>
-							<!-- Text input-->
-							<div class="form-group">
-								<label class="col-md-4 control-label" for="harga">Harga Kamar</label>
-								<div class="col-md-3">
-									<input id="harga" maxlength="20" name="harga" type="text" placeholder="" value="<?=set_value('harga')?>" class="form-control input-md" required="">
-								</div>
-								<label class="col-md-5" style="margin-top:5pt;">*tanpa titik ataupun koma</label>
-							</div>
-							<!-- Select Basic -->
-							<div class="form-group">
-								<label class="col-md-4 control-label" for="tahun">Tahun Jenis</label>
-								<div class="col-md-8">
-									<select id="tahun" name="tahun" class="form-control">
-										<?php for($i=$tahun; $i>=2000 ; $i--){ ?>
-										<option value="<?=$i?>"><?=$i?></option>
-										<?php } ?>
-									</select>
-								</div>
-							</div>
-							<!-- Multiple Checkboxes (inline) -->
-							<div class="form-group">
-								<label class="col-md-4 control-label" for="facilities">Fasilitas</label>
-								<div class="col-md-8">
-									<?php $i=1; foreach($facilities as $list): if($i>3) echo '<br/>';?>
-									<label class="checkbox-inline" for="facilities-<?=$i?>" style="margin-right:1pt;">
-										<input type="checkbox" name="facilities[]" id="facilities-<?=$i++?>" value="<?=$list['nama']?>">
-										<?=$list['nama']?>
-									</label>
-									<?php endforeach; ?>
-								</div>
-							</div>
-							<!-- Foto -->
-							<div class="form-group">
-								<label class="col-md-4 control-label" for="photo">Foto Kamar</label>
-								<div class="col-md-8">
-									<input id="photo" name="photo" class="input-file" type="file">
-								</div>
-							</div>
-							<!-- Button -->
-							<div class="form-group">
-								<label class="col-md-4 control-label" for="Submit"></label>
-								<div class="col-md-4">
-									<button id="Submit" name="Submit" class="btn btn-warning">Simpan</button>
-								</div>
-							</div>
-						</fieldset>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-	<script type="text/javascript">
-	function confirmDeleteModal(id){
-	$('#deleteModal').modal();
-		$('#deleteButton').html('<a class="btn btn-danger" onclick="deleteData('+id+')">Hapus Tamu</a>');
-	}
-	
-	function deleteData(id){
-	// do your stuffs with id
-	window.location.assign("<?=site_url('admin/jenis/delete/')?>"+id)
-	$('#deleteModal').modal('hide'); // now close modal
-	}
-	function detailData(id, group){
-	window.location.assign("<?=site_url('admin/jenis/details/')?>"+id)
-	}
-	function updateData(id, group){
-	window.location.assign("<?=site_url('admin/jenis/viewUpdate/')?>"+id)
-	}
-	</script>
-	<style type="text/css" media="screen">
-		.labelDetail {
-		margin-top: 5pt;
-		}
-		.detailPanel {
-		margin-left: 10px;
-		}
-	</style>
-	<div id="deleteModal" class="modal fade" role='dialog'>
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-					<h4 class="modal-title">Hapus Pengguna </h4>
-				</div>
-				<div class="modal-body">
-					<p>Anda yakin akan menghapus <strong> jenis kamar</strong> ini ?</p>
-					<p>Jenis ini tidak akan ditemukan pada transaksi yang mungkin akan terjadi di masa yang akan datang. Namun, histori tamu ini akan tetap ada di setiap transaksi yang pernah tercatat</p>
-					
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-					<span id= 'deleteButton'></span>
-				</div>
-				
-			</div>
-		</div>
-	</div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="jenis">Nama Jenis Kamar</label>
+                            <div class="col-md-8">
+                                <input id="jenis" maxlength="40" name="jenis" type="text" placeholder=""
+                                       value="<?= set_value('jenis') ?>" class="form-control input-md" required="">
+                            </div>
+                        </div>
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="harga">Harga Kamar</label>
+                            <div class="col-md-3">
+                                <input id="harga" maxlength="20" name="harga" type="text" placeholder=""
+                                       value="<?= set_value('harga') ?>" class="form-control input-md" required="">
+                            </div>
+                            <label class="col-md-5" style="margin-top:5pt;">*tanpa titik ataupun koma</label>
+                        </div>
+                        <!-- Select Basic -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="tahun">Tahun Jenis</label>
+                            <div class="col-md-8">
+                                <select id="tahun" name="tahun" class="form-control">
+                                    <?php for ($i = $tahun; $i >= 2000; $i--) { ?>
+                                        <option value="<?= $i ?>"><?= $i ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <!-- Multiple Checkboxes (inline) -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="facilities">Fasilitas</label>
+                            <div class="col-md-8">
+                                <?php $i = 1;
+                                foreach ($facilities as $list): if ($i % 3 == 1) echo '<br/>'; ?>
+                                    <label class="checkbox-inline" for="facilities-<?= $i ?>" style="margin-right:1pt;">
+                                        <input type="checkbox" name="facilities[]" id="facilities-<?= $i++ ?>"
+                                               value="<?= $list['nama'] ?>">
+                                        <?= $list['nama'] ?>
+                                    </label>
+                                <?php endforeach; ?>
+                            </div>
+                        </div>
+                        <!-- Foto -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="photo">Foto Kamar</label>
+                            <div class="col-md-8">
+                                <input id="photo" name="photo" class="input-file" type="file">
+                            </div>
+                        </div>
+                        <!-- Button -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="Submit"></label>
+                            <div class="col-md-4">
+                                <button id="Submit" name="Submit" class="btn btn-warning">Simpan</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    function confirmDeleteModal(id) {
+        $('#deleteModal').modal();
+        $('#deleteButton').html('<a class="btn btn-danger" onclick="deleteData(' + id + ')">Hapus Tamu</a>');
+    }
+
+    function deleteData(id) {
+        // do your stuffs with id
+        window.location.assign("<?=site_url('admin/jenis/delete/')?>" + id)
+        $('#deleteModal').modal('hide'); // now close modal
+    }
+    function detailData(id) {
+        $.ajax({
+            type: "POST",
+            url: "<?=site_url('admin/jenis/details')?>",
+            data: "id=" + id,
+            success: function (msg) {
+                $("#div_result").html(msg);
+            }
+        });
+    }
+    function updateData(id) {
+        $.ajax({
+            type: "POST",
+            url: "<?=site_url('admin/jenis/viewUpdate')?>",
+            data: "id=" + id,
+            success: function (msg) {
+                $("#div_result").html(msg);
+            }
+        });
+    }
+</script>
+<style type="text/css" media="screen">
+    .labelDetail {
+        margin-top: 5pt;
+    }
+
+    .detailPanel {
+        margin-left: 10px;
+    }
+</style>
+<div id="deleteModal" class="modal fade" role='dialog'>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title">Hapus Pengguna </h4>
+            </div>
+            <div class="modal-body">
+                <p>Anda yakin akan menghapus <strong> jenis kamar</strong> ini ?</p>
+                <p>Jenis ini tidak akan ditemukan pada transaksi yang mungkin akan terjadi di masa yang akan datang.
+                    Namun, histori tamu ini akan tetap ada di setiap transaksi yang pernah tercatat</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <span id='deleteButton'></span>
+            </div>
+        </div>
+    </div>
+</div>
