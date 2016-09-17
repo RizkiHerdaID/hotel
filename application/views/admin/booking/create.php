0de11ee91c551438b08c28269ce177e9e44edfd7
Rizki Herdatullah <rizkiherda@gmail.com>
2016-09-17 12:18:24 +0700

    Bug Fixed on Booking

--- application/views/admin/booking/create.php
+++ application/views/admin/booking/create.php
@@ -11,8 +11,8 @@
 							<h4>Data Tamu</h4>
 							<hr>
 							<h6>Cari Tamu Jika Tamu Pernah Check-in</h6>
-							<?php if(isset($tamu)){ ?>
-							<?php foreach($tamu as $list): ?>
+							<?php if (isset($tamu)) {?>
+							<?php foreach ($tamu as $list): ?>
 							<fieldset>
 								<!-- Text input-->
 								<input id="id" name="id" type="text" value="<?=$list['id']?>" required="" hidden />
@@ -34,15 +34,15 @@
 								<div class="form-group">
 									<label class="col-md-4 control-label" for="lname">Nama Belakang</label>
 									<div class="col-md-8">
-										<input id="lname" name="lname" type="text" value="<?=$list['nama_belakang']?>" placeholder="" class="form-control input-md" required="" disabled />
-										
+										<input id="lname" name="lname" type="text" value="<?=$list['nama_belakang']?>" placeholder="" class="form-control input-md" disabled />
+
 									</div>
 								</div>
 								<div class="form-group">
 									<label class="col-md-4 control-label" for="email">E-mail</label>
 									<div class="col-md-8">
 										<input id="email" name="email" type="text" value="<?=$list['email']?>" placeholder="" class="form-control input-md" required="" disabled />
-										
+
 									</div>
 								</div>
 								<!-- Text input-->
@@ -50,16 +50,16 @@
 									<label class="col-md-4 control-label" for="phone">Telepon / HP</label>
 									<div class="col-md-8">
 										<input id="phone" name="phone" type="text" value="<?=$list['telepon']?>" placeholder="" class="form-control input-md" required="" disabled />
-										
+
 									</div>
 								</div>
-								
+
 								<!-- Text input-->
 								<div class="form-group">
 									<label class="col-md-4 control-label" for="country">Negara</label>
 									<div class="col-md-8">
 										<input id="country" name="country" type="text" value="<?=$list['negara']?>" placeholder="" class="form-control input-md" required="" disabled />
-										
+
 									</div>
 								</div>
 								<!-- Text input-->
@@ -67,16 +67,16 @@
 									<label class="col-md-4 control-label" for="address">Alamat</label>
 									<div class="col-md-8">
 										<input id="address" name="address" type="text" value="<?=$list['alamat']?>" placeholder="" class="form-control input-md" required="" disabled />
-										
+
 									</div>
 								</div>
-								
+
 								<!-- Text input-->
 								<div class="form-group">
 									<label class="col-md-4 control-label" for="province">Provinsi</label>
 									<div class="col-md-8">
 										<input id="province" name="province" type="text" value="<?=$list['provinsi']?>" placeholder="" class="form-control input-md" required="" disabled />
-										
+
 									</div>
 								</div>
 								<!-- Text input-->
@@ -84,7 +84,7 @@
 									<label class="col-md-4 control-label" for="city">Kota / Kabupaten</label>
 									<div class="col-md-8">
 										<input id="city" name="city" type="text" value="<?=$list['kota']?>" placeholder="" class="form-control input-md" required="" disabled />
-										
+
 									</div>
 								</div>
 								<!-- Text input-->
@@ -92,7 +92,7 @@
 									<label class="col-md-4 control-label" for="zipcode">Kode Pos</label>
 									<div class="col-md-8">
 										<input id="zipcode" name="zipcode" type="text" value="<?=$list['zip']?>" placeholder="" class="form-control input-md" required="" disabled />
-										
+
 									</div>
 								</div>
 								<!-- Select Basic -->
@@ -104,7 +104,7 @@
 								</div>
 								<!-- Button -->
 							</fieldset>
-							<?php endforeach; ?>
+							<?php endforeach;?>
 							<?php } else {?>
 							<fieldset>
 								<!-- Text input-->
@@ -121,21 +121,21 @@
 									<label class="col-md-4 control-label" for="fname">Nama Depan</label>
 									<div class="col-md-8">
 										<input id="fname" name="fname" type="text" value="<?=set_value('fname')?>" placeholder="" class="form-control input-md" required="">
-										
+
 									</div>
 								</div>
 								<div class="form-group">
 									<label class="col-md-4 control-label" for="lname">Nama Belakang</label>
 									<div class="col-md-8">
 										<input id="lname" name="lname" type="text" value="<?=set_value('lname')?>" placeholder="" class="form-control input-md" required="">
-										
+
 									</div>
 								</div>
 								<div class="form-group">
 									<label class="col-md-4 control-label" for="email">E-mail</label>
 									<div class="col-md-8">
 										<input id="email" name="email" type="text" value="<?=set_value('email')?>" placeholder="" class="form-control input-md" required="">
-										
+
 									</div>
 								</div>
 								<!-- Text input-->
@@ -143,7 +143,7 @@
 									<label class="col-md-4 control-label" for="phone">Telepon / HP</label>
 									<div class="col-md-8">
 										<input id="phone" name="phone" type="text" value="<?=set_value('phone')?>" placeholder="" class="form-control input-md" required="">
-										
+
 									</div>
 								</div>
 								<!-- Text input-->
@@ -151,7 +151,7 @@
 									<label class="col-md-4 control-label" for="country">Negara</label>
 									<div class="col-md-8">
 										<input id="country" name="country" type="text" value="<?=set_value('country')?>" placeholder="" class="form-control input-md" required="">
-										
+
 									</div>
 								</div>
 								<!-- Text input-->
@@ -159,16 +159,16 @@
 									<label class="col-md-4 control-label" for="address">Alamat</label>
 									<div class="col-md-8">
 										<input id="address" name="address" type="text" value="<?=set_value('address')?>" placeholder="" class="form-control input-md" required="">
-										
+
 									</div>
 								</div>
-								
+
 								<!-- Text input-->
 								<div class="form-group">
 									<label class="col-md-4 control-label" for="province">Provinsi</label>
 									<div class="col-md-8">
 										<input id="province" name="province" type="text" value="<?=set_value('province')?>" placeholder="" class="form-control input-md" required="">
-										
+
 									</div>
 								</div>
 								<!-- Text input-->
@@ -176,7 +176,7 @@
 									<label class="col-md-4 control-label" for="city">Kota / Kabupaten</label>
 									<div class="col-md-8">
 										<input id="city" name="city" type="text" value="<?=set_value('city')?>" placeholder="" class="form-control input-md" required="">
-										
+
 									</div>
 								</div>
 								<!-- Text input-->
@@ -184,7 +184,7 @@
 									<label class="col-md-4 control-label" for="zipcode">Kode Pos</label>
 									<div class="col-md-8">
 										<input id="zipcode" name="zipcode" type="text" value="<?=set_value('zipcode')?>" placeholder="" class="form-control input-md" required="">
-										
+
 									</div>
 								</div>
 								<!-- Select Basic -->
@@ -192,14 +192,14 @@
 									<label class="col-md-4 control-label" for="gcode">Grup Tamu</label>
 									<div class="col-md-8">
 										<select id="gcode" name="gcode" class="form-control">
-											<?php foreach($grup as $list): ?>
+											<?php foreach ($grup as $list): ?>
 											<option value="<?=$list['id_guest_group']?>"><?=$list['nama']?></option>
-											<?php endforeach; ?>
+											<?php endforeach;?>
 										</select>
 									</div>
 								</div>
 							</fieldset>
-							<?php } ?>
+							<?php }?>
 						</div>
 						<div class=" col-md-6 col-lg-6">
 							<h4>Data Kamar</h4>
@@ -209,17 +209,18 @@
 								<label class="col-md-4 control-label" for="jenis">Jenis Kamar</label>
 								<div class="col-md-8">
 									<select id="jenis" name="jenis" class="form-control">
-										<?php foreach($jenis as $list): ?>
-										<?php if($this->session->flashdata('jenis') != NULL){
-											$tmp = $this->session->flashdata('jenis');
-										if ($list['idclass'] == $tmp) { ?>
+										<?php foreach ($jenis as $list): ?>
+										<?php if ($this->session->flashdata('jenis') != NULL) {
+	$tmp = $this->session->flashdata('jenis');
+	if ($list['idclass'] == $tmp) {?>
 										<option value="<?=$list['idclass']?>" selected><?=$list['title']?></option>
 										<?php } else {?>
 										<option value="<?=$list['idclass']?>"><?=$list['title']?></option>
 										<?php }
-										} else { ?>
+} else {?>
 										<option value="<?=$list['idclass']?>"><?=$list['title']?></option>
-										<?php } endforeach; ?>
+										<?php }
+endforeach;?>
 									</select>
 								</div>
 							</div>
